b4459562a9664ccaf9f7be3b568e7f820b44068e
wangjin <wangjin1904@163.com>
2017-11-29 21:09:35 +0000

    add code of dqn

--- DQN/deepQlearning.py
+++ DQN/deepQlearning.py
@@ -138,13 +138,14 @@ class Estimator():
 
         if self.summary_writer:
             self.summary_writer.add_summary(summaries, global_step)
+
         return loss
 
 # For Testing ....
 tf.reset_default_graph()
 global_step = tf.Variable(0, name="global_step", trainable=False)
 
-e = Estimator(scope="test")
+e = Estimator(scope="test", summaries_dir="./testData")
 sp = StateProcessor()
 
 with tf.Session() as sess:
@@ -164,3 +165,279 @@ with tf.Session() as sess:
     y = np.array([10.0, 10.0])
     a = np.array([1, 3])
     print(e.update(sess, observations, a, y))
+
+class ModelParametersCopier():
+    """
+    Copy model parameters of one estimator to another
+    """
+
+    def __init__(self, estimator1, estimator2):
+        """
+        Defines copy-work operation graph.
+        :param estimator1: Estimator to copy the parameters from
+        :param estimator2: Estimator to copy the parameters to
+        """
+        e1_params = [t for t in tf.trainable_variables() if t.name.startswith(estimator1.scope)]
+        e1_params = sorted(e1_params, key=lambda v: v.name)
+        e2_params = [t for t in tf.trainable_variables() if t.name.startswith(estimator2.scope)]
+        e2_params = sorted(e2_params, key=lambda v: v.name)
+
+        self.update_ops = []
+        for e1_v, e2_v in zip(e1_params, e2_params):
+            op = e2_v.assign(e1_v)
+            self.update_ops.append(op)
+
+    def make(self, sess):
+        """
+        Makes copy.
+        Args:
+            sess: Tensorflow session instance
+        """
+        sess.run(self.update_ops)
+
+def make_epsilon_greedy_policy(estimator, nA):
+    """
+    Creates an epsilon-greedy policy based on a given Q-function approximator and epsilon.
+
+    Args:
+        estimator: An estimator that returns q values for a given state
+        nA: Number of actions in the environment.
+
+    Returns:
+        A function that takes the (sess, observation, epsilon) as an argument and returns
+        the probabilities for each action in the form of a numpy array of length nA.
+
+    """
+    def policy_fn(sess, observation, epsilon):
+        A = np.ones(nA, dtype=float) * epsilon / nA
+        q_values = estimator.predict(sess, np.expand_dims(observation, 0))[0]
+        best_action = np.argmax(q_values)
+        A[best_action] += (1.0 - epsilon)
+        return A
+    return policy_fn
+
+
+def deep_q_learning(sess,
+                    env,
+                    q_estimator,
+                    target_estimator,
+                    state_processor,
+                    num_episodes,
+                    experiment_dir,
+                    replay_memory_size=500000,
+                    replay_memory_init_size=50000,
+                    update_target_estimator_every=10000,
+                    discount_factor=0.99,
+                    epsilon_start=1.0,
+                    epsilon_end=0.1,
+                    epsilon_decay_steps=500000,
+                    batch_size=32,
+                    record_video_every=50):
+    """
+    Q-Learning algorithm for off-policy TD control using Function Approximation.
+    Finds the optimal greedy policy while following an epsilon-greedy policy.
+
+    :param sess: Tensorflow Session object
+    :param env: OpenAI environment
+    :param q_estimator: Estimator object used for the q values
+    :param target_estimator: Estimator object used for the targets
+    :param state_processor: A StateProcessor object
+    :param num_episodes: Number of episodes to run
+    :param experiment_dir: Directory to save Tensorflow summaries in
+    :param replay_memory_size: Size of the replay memory
+    :param replay_memory_init_size: Number of random experiences to sample when initializing the reply memory
+    :param update_target_estimator_every: Copy parameters from the Q estimator to the
+            target estimator every N steps
+    :param discount_factor: Lambda time discount factor
+    :param epsilon_start: Chance to sample a random action when taking an action.
+                Epsilon is decayed over time and this is the start value
+    :param epsilon_end: The final minimum value of epsilon after decaying is done
+    :param epsilon_decay_steps: Number of steps to decay epsilon over
+    :param batch_size: Size of batches to sample from the replay memory
+    :param record_video_every: Record a video every N episode
+
+    :return:
+        An EpisodeStats object with two numpy arrays for episode_lengths and episode_rewards.
+    """
+    Transition = namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
+
+    # The replay memory
+    replay_memory = []
+
+    # Make model copier object
+    estimator_copy = ModelParametersCopier(q_estimator, target_estimator)
+
+    # Keeps track of useful statistics
+    stats = plotting.EpisodeStats(
+        episode_lengths=np.zeros(num_episodes),
+        episode_rewards=np.zeros(num_episodes))
+
+    # For 'system/' summaries, useful to check if current process looks healthy
+    current_process = psutil.Process()
+
+    # Create directories for checkpoints and summaries
+    checkpoint_dir = os.path.join(experiment_dir, "checkpoints")
+    checkpoint_path = os.path.join(checkpoint_dir, "model")
+    monitor_path = os.path.join(experiment_dir, "monitor")
+
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+    if not os.path.exists(monitor_path):
+        os.makedirs(monitor_path)
+
+    saver = tf.train.Saver()
+    # Load a previous checkpoint if we find one
+    latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
+    if latest_checkpoint:
+        print("Loading model checkpoint {}...\n".format(latest_checkpoint))
+        saver.restore(sess, latest_checkpoint)
+
+    # Get the current time step
+    total_t = sess.run(tf.contrib.framework.get_global_step())
+
+    # The epsilon decay schedule
+    epsilons = np.linspace(epsilon_start, epsilon_end, epsilon_decay_steps)
+
+    # The policy we're following
+    policy = make_epsilon_greedy_policy(
+        q_estimator,
+        len(VALID_ACTIONS))
+
+    # Populate the replay memory with initial experience
+    print("Populating replay memory...")
+    state = env.reset()
+    state = state_processor.process(sess, state)
+    state = np.stack([state] * 4, axis=2)
+
+    for i in range(replay_memory_init_size):
+        action_probs = policy(sess, state, epsilons[min(total_t, epsilon_decay_steps-1)])
+        action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+        next_state, reward, done, _ = env.step(VALID_ACTIONS[action])
+        next_state = state_processor.process(sess, next_state)
+        next_state = np.append(state[:,:,1:], np.expand_dims(next_state,2), axis=2)
+        replay_memory.append(Transition(state, action, reward, next_state, done))
+
+        if done:
+            state = env.reset()
+            state = state_processor.process(sess, state)
+            state = np.stack([state]*4, axis=2)
+        else:
+            state = next_state
+
+    # Record videos
+    # Add env Monitor wrapper
+    env = Monitor(env, directory=monitor_path, video_callable=lambda count: count % record_video_every == 0, resume=True)
+
+    for i_episode in range(num_episodes):
+        # Save the current checkpoint
+        saver.save(tf.get_default_session(), checkpoint_path)
+
+        # Reset the environment
+        state = env.reset()
+        state = state_processor.process(sess, state)
+        state = np.stack([state] * 4, axis=2)
+        loss = None
+
+        # One step in the environment
+        for t in itertools.count():
+            # Epsilon for this time step
+            epsilon = epsilons[min(total_t, epsilon_decay_steps-1)]
+
+            # Maybe update the target estimator
+            if total_t % update_target_estimator_every == 0:
+                estimator_copy.make(sess)
+                print("\nCopied model parameters to target network.")
+
+            # print out which step we're on, useful for debugging.
+            print("\rStep {} ({}) @ Episode {}/{}, loss: {}".format(
+                t, total_t, i_episode + 1, num_episodes, loss), end="")
+            sys.stdout.flush()
+
+            # Take a step
+            action_probs = policy(sess, state, epsilon)
+            action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+            next_state, reward, done, _ = env.step(VALID_ACTIONS[action])
+            next_state = state_processor.process(sess, next_state)
+            next_state = np.append(state[:,:,1:], np.expand_dims(next_state, 2), axis=2)
+
+            # If our replay memory is full, pop the first element
+            if len(replay_memory) == replay_memory_size:
+                replay_memory.pop(0)
+
+            # Save transition to replay memory
+            replay_memory.append(Transition(state, action, reward, next_state, done))
+
+            # Update statistics
+            stats.episode_rewards[i_episode] += reward
+            stats.episode_lengths[i_episode] = t
+
+            # Sample a minibatch from the replay memory
+            samples = random.sample(replay_memory, batch_size)
+            states_batch, action_batch, reward_batch, next_states_batch, done_batch = map(np.array, zip(*samples))
+
+            # Calculate q values and targets
+            q_values_next = target_estimator.predict(sess, next_states_batch)
+            targets_batch = reward_batch + np.invert(done_batch).astype(np.float32) * discount_factor * np.amax(q_values_next, axis=1)
+
+            # Perform gradient descent update
+            states_batch = np.array(states_batch)
+            loss =  q_estimator.update(sess, states_batch, action_batch, targets_batch)
+
+            if done:
+                break
+
+            state = next_state
+            total_t += 1
+
+            # Add summaries to tensorboard
+            episode_summary = tf.Summary()
+            episode_summary.value.add(simple_value=epsilon, tag="episode/epsilon")
+            episode_summary.value.add(simple_value=stats.episode_rewards[i_episode], tag="episode/reward")
+            episode_summary.value.add(simple_value=stats.episode_lengths[i_episode], tag="episode/length")
+            episode_summary.value.add(simple_value=current_process.cpu_percent(), tag="system/cpu_usage_percent")
+            episode_summary.value.add(simple_value=current_process.memory_percent(memtype="vms"),
+                                      tag="system/v_memeory_usage_percent")
+            q_estimator.summary_writer.add_summary(episode_summary, i_episode)
+            q_estimator.summary_writer.flush()
+
+            yield total_t, plotting.EpisodeStats(
+                episode_lengths=stats.episode_lengths[:i_episode + 1],
+                episode_rewards=stats.episode_rewards[:i_episode + 1])
+
+        return stats
+
+
+tf.reset_default_graph()
+
+# Where we save our checkpoints and graphs
+experiment_dir = os.path.abspath("./experiments/{}".format(env.spec.id))
+
+# Create a glboal step variable
+global_step = tf.Variable(0, name='global_step', trainable=False)
+
+# Create estimators
+q_estimator = Estimator(scope="q_estimator", summaries_dir=experiment_dir)
+target_estimator = Estimator(scope="target_q")
+
+# State processor
+state_processor = StateProcessor()
+
+# Run it!
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for t, stats in deep_q_learning(sess,
+                                    env,
+                                    q_estimator=q_estimator,
+                                    target_estimator=target_estimator,
+                                    state_processor=state_processor,
+                                    experiment_dir=experiment_dir,
+                                    num_episodes=10000,
+                                    replay_memory_size=500000,
+                                    replay_memory_init_size=50000,
+                                    update_target_estimator_every=10000,
+                                    epsilon_start=1.0,
+                                    epsilon_end=0.1,
+                                    epsilon_decay_steps=500000,
+                                    discount_factor=0.99,
+                                    batch_size=32):
+        print("\nEpisode Reward: {}".format(stats.episode_rewards[-1]))
